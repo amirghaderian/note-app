5d78d02a2f1e89aaf0f415b3fd3201d7dc16f31d
asd <asd.it@gmail.com>
2023-09-25 00:40:16 +0330

    Save form data in state and log it

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,17 +1,44 @@
+import { useState } from "react";
+
 const AddNewNote = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const handleSubmite = (e) => {
-    e.preventDefault()
-    console.log(e);
+    e.preventDefault();
+    const newNote = {
+      title,
+      description,
+      id: Date.now(),
+      complited: false,
+      createdAt: new Date().toISOString(),
+    };
+    setTitle("")
+    setDescription("")
+    console.log(newNote);
   };
+  const handleChange = (e) => setTitle(e.target.value);
   return (
     <div className="add-new-note">
       <h2>Add New Note</h2>
       <form className="note-form" onSubmit={handleSubmite}>
-        <input type="text" className="text-field" placeholder="Note title ..." />
-        <input type="text" className="text-field" placeholder="Note description ..."/>
-        <button type="submit" className="btn btn--primary">Add New Note</button>
+        <input
+          value={title}
+          onChange={handleChange}
+          type="text"
+          className="text-field"
+          placeholder="Note title ..."
+        />
+        <input
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="Note description ..."
+        />
+        <button type="submit" className="btn btn--primary">
+          Add New Note
+        </button>
       </form>
-      
     </div>
   );
 };
